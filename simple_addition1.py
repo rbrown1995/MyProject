bdf61b0b5d1c52a48cc9dbbccf6837f2229212b6
Rachel Brown <rebrown@wpi.edu>
2015-06-02 14:31:03 -0400

    Added simple_addition1.py

---
+++ simple_addition1.py
@@ -0,0 +1,14 @@
+def simple_addition():
+	#input numbers to be added
+	num_1 = input('enter first number')
+	
+	num_2 = input('enter second number')
+
+	#add the two numbers together
+	summation = float(num_1) + float(num_2)
+
+	#show summation
+	print (summation)
+	
+
+simple_addition()
